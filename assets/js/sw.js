3144681da49149f505464dac64aa7cf5d48e8313
Ruben Lopez <rubenlop88@gmail.com>
2018-08-16 19:26:09 -0400

    enable comments

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -28,7 +28,7 @@ const SHELL_CACHE = "shell-8.0.0--v1--sw/";
 const ASSETS_CACHE = "assets--v1--sw/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2018-08-16T19:04:55-04:00--sw/";
+const CONTENT_CACHE = "content--2018-08-16T19:25:43-04:00--sw/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const CACHE_SEARCH_PARAM = "sw-cache";
