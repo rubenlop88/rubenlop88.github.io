21caa4b8f81565c5a44b79000c49242d481a91ec
Ruben Lopez <rubenlop88@gmail.com>
2025-07-01 21:41:13 -0300

    Add jaspr project source files.

---
+++ source/lib/jaspr_options.dart
@@ -0,0 +1,42 @@
+// dart format off
+// ignore_for_file: type=lint
+
+// GENERATED FILE, DO NOT MODIFY
+// Generated with jaspr_builder
+
+import 'package:jaspr/jaspr.dart';
+import 'package:source/components/counter.dart' as prefix0;
+import 'package:source/components/header.dart' as prefix1;
+import 'package:source/pages/about.dart' as prefix2;
+import 'package:source/pages/home.dart' as prefix3;
+import 'package:source/app.dart' as prefix4;
+
+/// Default [JasprOptions] for use with your jaspr project.
+///
+/// Use this to initialize jaspr **before** calling [runApp].
+///
+/// Example:
+/// ```dart
+/// import 'jaspr_options.dart';
+///
+/// void main() {
+///   Jaspr.initializeApp(
+///     options: defaultJasprOptions,
+///   );
+///
+///   runApp(...);
+/// }
+/// ```
+JasprOptions get defaultJasprOptions => JasprOptions(
+  clients: {
+    prefix2.About: ClientTarget<prefix2.About>('pages/about'),
+
+    prefix3.Home: ClientTarget<prefix3.Home>('pages/home'),
+  },
+  styles: () => [
+    ...prefix0.CounterState.styles,
+    ...prefix1.Header.styles,
+    ...prefix2.About.styles,
+    ...prefix4.App.styles,
+  ],
+);
