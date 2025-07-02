d1c90bf2116688437e796c785c943d750b0defc8
Ruben Lopez <rubenlop88@gmail.com>
2025-07-02 01:28:32 -0300

    Move jaspr project to top level directory

--- lib/main.dart
+++ lib/main.dart
@@ -15,27 +15,7 @@ void main() {
 
   runApp(
     Document(
-      title: 'source',
-      styles: [
-        // Special import rule to include to another css file.
-        css.import('https://fonts.googleapis.com/css?family=Roboto'),
-        // Each style rule takes a valid css selector and a set of styles.
-        // Styles are defined using type-safe css bindings and can be freely chained and nested.
-        css('html, body').styles(
-          width: 100.percent,
-          minHeight: 100.vh,
-          padding: Padding.zero,
-          margin: Margin.zero,
-          fontFamily: const FontFamily.list(
-              [FontFamily('Roboto'), FontFamilies.sansSerif]),
-        ),
-        css('h1').styles(
-          margin: Margin.unset,
-          fontSize: 4.rem,
-        ),
-      ],
       head: [
-        // The generated flutter manifest and bootstrap script.
         link(rel: 'manifest', href: 'manifest.json'),
         script(src: "flutter_bootstrap.js", async: true, []),
       ],
@@ -44,7 +24,7 @@ void main() {
         layouts: [
           DocsLayout(
             header: Header(
-              title: 'Jaspr',
+              title: 'Ruben Lopez',
               logo:
                   'https://raw.githubusercontent.com/schultek/jaspr/refs/heads/main/assets/logo.png',
               items: [
